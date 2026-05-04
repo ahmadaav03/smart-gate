6da7c8b51f5c9cedd8d88445964d19af8d0b26bd
Ahmad Vawda <ahmad.aav03@gmail.com>
2026-05-04 21:12:39 +0200

    login & Auth 6

--- middleware.ts
+++ middleware.ts
@@ -2,15 +2,6 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  const token = request.cookies.get("sb-xrxhqfsscqokkavleemy-auth-token");
-  const hasSession = !!token;
-
-  if (request.nextUrl.pathname.includes("/dashboard")) {
-    if (!hasSession) {
-      return NextResponse.redirect(new URL("/resident/login", request.url));
-    }
-  }
-
   return NextResponse.next();
 }
 
